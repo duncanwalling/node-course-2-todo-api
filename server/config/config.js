f786fa882b36a8a5f4d07960602a1bdfa3735b00
Duncan Walling <duncanwalling@gmail.com>
2018-09-20 17:22:38 +0100

    Set heroku mongo config

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,8 @@ if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'var uri = "mongodb://fourteenfish:dU2wmp3nMnnkksz@cluster0-shard-00-00-zn00d.mongodb.net:27017,cluster0-shard-00-01-zn00d.mongodb.net:27017,cluster0-shard-00-02-zn00d.mongodb.net:27017/Testing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 }
+else
+{
+    // therefore production
+    process.env.MONGODB_URI = 'var uri = "mongodb://fourteenfish:dU2wmp3nMnnkksz@cluster0-shard-00-00-zn00d.mongodb.net:27017,cluster0-shard-00-01-zn00d.mongodb.net:27017,cluster0-shard-00-02-zn00d.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+}
